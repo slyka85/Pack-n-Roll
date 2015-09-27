8d78685d18aae1d2dcff18e2a587f9793b328844
Anya Melnyk <gannasachakov@gmail.com>
2015-09-27 19:06:54 -0400

    changed form, button, activity ids passed

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,15 +3,12 @@ class TripsController < ApplicationController
 
   def index
     @trips = Trip.where(:user_id == current_user.id)
-    @trips_by_date = @trips.group_by{|x| x.start_date.to_date} 
+    @trips_by_date = current_user.trips.group_by{|x| x.start_date.to_date} 
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @color = "#" + "%06x" % (rand * 0xffffff)
-    # respond_to do |format| 
-    #   format.html
-    #   format.json { render :json => @trips } 
-    # end
- 
   end
+
+
 
   def update_default_items
     @trip = Trip.find(params[:id])
@@ -19,61 +16,54 @@ class TripsController < ApplicationController
     redirect_to @trip
   end
 
+
+
   def create
-    @trip = Trip.new(params[:trip])
-    @trip.user_id = current_user.id
-    @trip.destination = params[:destination]
-    @trip.start_date = params[:start_date]
-    @trip.end_date = params[:end_date]
-    @trip.id = params[:id]
-    trip_id = params[:id]
+    # binding.pry
+    # @trip = Trip.new(params[:trip])
+    # @trip.user_id = current_user.id
+    # @trip.destination = params[:destination]
+    # @trip.start_date = params[:start_date]
+    # @trip.end_date = params[:end_date]
+    # @trip.id = params[:id]
+    # trip_id = params[:id]
+      @trip = Trip.create(
+      destination: params["trip"]["destination"],
+      start_date: params["trip"]["start_date"],
+      end_date: params["trip"]["end_date"],
+      user_id: current_user.id,
+      activity_item_id: params["activity_ids"])
+
     # @trip.save
     # @activity_ids = params["activity_ids"].map { |aid| aid}
     # @activity_ids.each do |aid|
-    #    @activity = ActivityItem.find_by_id(aid)
-    #    @activity.trip_id = Trip.find_by_id(@trip.id).id
-    #    @trip.activity_item_id = @activity.id
-    #    @activity.save
-    #    @trip.save
-    #  end
+       # @activity = ActivityItem.find_by_id(aid)
+       # @activity.trip_id = Trip.find_by_id(@trip.id).id
+       # @trip.activity_item_id = @activity.id
+       # @activity.save
+       #@trip.save
+     # end
 
     if @trip.save
-      redirect_to(@trip)
+      redirect_to trip_path(@trip, :activity => params[:activity_ids])
     else
       flash[:alert] = 'Please fill out all the fields to proceed'
       redirect_to root_path
     end
   end
 
+
+
   def activity_items_trip
   end
 
+
+
   def show
-    @params1 = params
-    @params2 = params[:default_trips]
-    @default_item_list = DefaultItem.new(params[:default_item])
-
+    # binding.pry
     @trip = Trip.find(params[:id])
-    trip_id = params[:id]
-    @default_item = DefaultItem.find(params[:id])
-    @default_clothes_items = DefaultItem.where(category: 'Clothes')
-    @all_default_items = DefaultItem.all
-    di_id = []
-    di_name = []
-
-    @all_default_items.each do |di|
-      di_id.push(di.id)
-      di_name.push(di.item)
-    end
-
-    default_item_id = @default_item.id
-
-    @default_paperwork_items = DefaultItem.where(category: 'Travel Paperwork')
-    @default_toiletries_items = DefaultItem.where(category: 'Toiletries')
-    @default_electronics_items = DefaultItem.where(category: 'Electronics')
-    @default_carryon_items = DefaultItem.where(category: 'Carry-on')
-    @default_misc_items = DefaultItem.where(category: 'Miscellaneous')
-
+    @activity_id = params[:activity]
+    @activity_chosen =  ActivityItem.where(id: @activity_id)
     @trip.default_items.pluck(:category).uniq.each do |category|
       puts category
       @trip.default_items.where(category: category).each do |default_item|
@@ -97,24 +87,23 @@ class TripsController < ApplicationController
     redirect_to saved_items_trip_path
     end
 
-  def saved_items_trip
-    @saved_items_trip = Trip.find(params[:id])
-    @saved_items = @saved_items_trip.default_items.order('id ASC')
-    @saved_toiletries = @saved_items_trip.default_items.where(category: 'Toiletries')
-    @saved_clothes = @saved_items_trip.default_items.where(category: 'Clothes')
-    @saved_paperwork = @saved_items_trip.default_items.where(category: 'Travel Paperwork')
-    @saved_electronics = @saved_items_trip.default_items.where(category: 'Electronics')
-    @saved_carry_on = @saved_items_trip.default_items.where(category: 'Carry-on')
-    @saved_misc = @saved_items_trip.default_items.where(category: 'Miscellaneous')
-  end
+  # def saved_items_trip
+  #   @saved_items_trip = Trip.find(params[:id])
+  #   @saved_items = @saved_items_trip.default_items.order('id ASC')
+  #   @saved_toiletries = @saved_items_trip.default_items.where(category: 'Toiletries')
+  #   @saved_clothes = @saved_items_trip.default_items.where(category: 'Clothes')
+  #   @saved_paperwork = @saved_items_trip.default_items.where(category: 'Travel Paperwork')
+  #   @saved_electronics = @saved_items_trip.default_items.where(category: 'Electronics')
+  #   @saved_carry_on = @saved_items_trip.default_items.where(category: 'Carry-on')
+  #   @saved_misc = @saved_items_trip.default_items.where(category: 'Miscellaneous')
+  # end
 
-  def delete_saved_items_trip
-    @saved_items_trip = Trip.find(params[:id])
-    @saved_items_trip.default_items.clear
-  end
+  # def delete_saved_items_trip
+  #   @saved_items_trip = Trip.find(params[:id])
+  #   @saved_items_trip.default_items.clear
+  # end
 
-  def packed
-  end
+
 
   def edit
     @trip = Trip.find(params[:id])
