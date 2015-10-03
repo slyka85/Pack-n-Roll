81c86d8f18cfb7da0553a68af136fe378de433a6
Anya Melnyk <gannasachakov@gmail.com>
2015-10-02 20:03:36 -0400

    testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,23 +8,23 @@ module ApplicationHelper
     image_tag("https://maps.googleapis.com/maps/api/staticmap?center=#{destination}&zoom=14&size=300x300", options)
   end
 
-  def current_location_by_ip
-  		GeoIp.timeout = 5
-  	  @location_request = GeoIp.geolocation(request.remote_ip)
-  	  @city = @location_request[:city]
-  	  @state = @location_request[:region_name]
-  	  @country = @location_request[:country_name]
-  	  @location_string = @city+", "+@state+", "+@country
-  	  return @location_string
-  end
+  # def current_location_by_ip
+  # 		GeoIp.timeout = 10
+  # 	  @location_request = GeoIp.geolocation(request.remote_ip)
+  # 	  @city = @location_request[:city]
+  # 	  @state = @location_request[:region_name]
+  # 	  @country = @location_request[:country_name]
+  # 	  @location_string = @city+", "+@state+", "+@country
+  # 	  return @location_string
+  # end
 
-  def location_with_geocoder
-  	@result = request.location
-  	@city = @result.data["city"]
-  	@state = @result.data["region_name"]
-  	@country = @result.data["country_name"]
-  	@full_location = @city+", "+@state+", "+@country
-  	return @full_location
-  end
+  # def location_with_geocoder
+  # 	@result = request.location
+  # 	@city = @result.data["city"]
+  # 	@state = @result.data["region_name"]
+  # 	@country = @result.data["country_name"]
+  # 	@full_location = @city+", "+@state+", "+@country
+  # 	return @full_location
+  # end
 
 end
