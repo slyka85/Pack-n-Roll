5518e75d512b959c813cf9bc476d54ae004afbe2
Anya Melnyk <gannasachakov@gmail.com>
2015-10-18 20:00:19 -0400

    changed landing page, added font

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module PackingList
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
